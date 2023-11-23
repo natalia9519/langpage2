1579f8410c1deeaaf64ee269d38956a61a9beccb
natalia9519 <nataliayda@gmail.com>
2023-11-23 22:46:30 +0100

    new file:   Easteregg.js 	deleted:    Fondocolor.js 	deleted:    imagenes/fondolove.avif 	deleted:    imagenes/kawaii.avif 	modified:   nosotros.html

---
+++ Easteregg.js
@@ -0,0 +1,8 @@
+
+function mostrarModal() {
+    document.getElementById('modal').style.display = "flex";
+  }
+  
+  function cerrarModal() {
+    document.getElementById('modal').style.display = "none";
+  }
