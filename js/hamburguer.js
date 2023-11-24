60483aecf5480e69c7085307775ee7f8fcb7c090
natalia9519 <nataliayda@gmail.com>
2023-11-24 09:42:50 +0100

    renamed:    navfot.css -> CSS/navfot.css 	renamed:    style.css -> CSS/style.css 	deleted:    hamburguer.js 	renamed:    Ofertas langpage.html -> html/Ofertas langpage.html 	renamed:    formulario.html -> html/formulario.html 	renamed:    login.html -> html/login.html 	renamed:    nosotros.html -> html/nosotros.html 	modified:   index.html 	renamed:    Easteregg.js -> js/Easteregg.js 	renamed:    form.js -> js/form.js 	new file:   js/hamburguer.js 	renamed:    heart.js -> js/heart.js 	renamed:    slider.js -> js/slider.js

---
+++ js/hamburguer.js
@@ -0,0 +1,23 @@
+
+ document.addEventListener('DOMContentLoaded', function() {
+   let showBtn = document.querySelector(".show");
+   let hiddenList = document.querySelector(".hidden-list");
+  
+
+   showBtn.addEventListener('click', function() {
+     if (hiddenList.style.display === 'none') {
+       hiddenList.style.display = 'block';
+     } else {
+       hiddenList.style.display = 'none';
+     }
+   });
+
+  document.addEventListener('click', function(event) {
+     if (event.target !== showBtn && !showBtn.contains(event.target) && event.target !== hiddenList && !hiddenList.contains(event.target)) {
+      hiddenList.style.display = 'none';
+     }
+  });
+ });
+
+
+
